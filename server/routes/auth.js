5bb03d757748563f897c5c048943d91268a41d27
Renier Arias <renier.arias@gmail.com>
2024-05-21 17:05:56 -0300

    client setup

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,25 +6,15 @@ import { requireSignIn } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.get("/", requireSignIn, authController.welcome);
-
 router.post("/pre-register", authController.preRegister);
-
 router.post("/register", authController.register);
-
 router.post("/login", authController.login);
-
 router.post("/forgot-password", authController.forgotPassword);
-
 router.put("/update-password", requireSignIn, authController.updatePassword);
-
 router.post("/access-account", authController.accessAccount);
-
 router.get("/refresh-token", authController.refreshToken);
-
 router.get("/current-user", requireSignIn, authController.currentUser);
-
 router.put("/update-profile", requireSignIn, authController.updateProfile);
-
 router.get("/profile/:username", authController.publicProfile);
 
 export default router;
